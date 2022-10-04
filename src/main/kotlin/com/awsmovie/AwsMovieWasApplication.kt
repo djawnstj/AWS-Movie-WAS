24112d3da6b09186ee5340ab2bb422cc8af7e7e0
junseo <djawnstj44@naver.com>
2022-10-04 21:35:26 +0900

    Create: WAS 프로젝트 생성
    Springboot 2.7, kotlin

---
+++ src/main/kotlin/com/awsmovie/AwsMovieWasApplication.kt
@@ -0,0 +1,11 @@
+package com.awsmovie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsMovieWasApplication
+
+fun main(args: Array<String>) {
+    runApplication<AwsMovieWasApplication>(*args)
+}
---
+++ src/test/kotlin/com/awsmovie/AwsMovieWasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.awsmovie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsMovieWasApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
