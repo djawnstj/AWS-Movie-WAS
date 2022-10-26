d4ebc344f052978d19dad27f9f789049a4e1b660
JUNSEO <djawnstj44@naver.com>
2022-10-26 17:14:44 +0900

    Feat: MovieDto 내 GenreDto 배열 추가
    MovieDto 내 GenreDto 배열 추가

--- src/main/kotlin/com/awsmovie/dto/movie/MovieDto.kt
+++ src/main/kotlin/com/awsmovie/dto/movie/MovieDto.kt
@@ -1,5 +1,6 @@
 package com.awsmovie.dto.movie
 
+import com.awsmovie.dto.movie.genre.GenreDto
 import java.time.LocalDateTime
 
 data class MovieDto(
@@ -8,6 +9,7 @@ data class MovieDto(
     val openingDate: LocalDateTime,
     val summary: String,
     val genreCode: List<Int> = ArrayList(),
+    val genres: List<GenreDto> = ArrayList(),
     val movieImagePath: String? = null,
     val rates: List<MovieRateDto> = ArrayList(),
 )
