f676411b1a641874569c9115834d087a80bee247
junseo <djawnstj44@naver.com>
2022-10-30 15:48:16 +0900

    Feat: MovieRateForm 필드 추가
    MovieRateForm movieId 필드 추가

--- src/main/kotlin/com/awsmovie/form/movie/MovieRateForm.kt
+++ src/main/kotlin/com/awsmovie/form/movie/MovieRateForm.kt
@@ -3,7 +3,8 @@ package com.awsmovie.form.movie
 import com.awsmovie.form.user.UserForm
 
 data class MovieRateForm(
-    val user: UserForm,
+    val user: UserForm?,
+    val movieId: Long,
     val rate: Int,
     val comment: String
 )
